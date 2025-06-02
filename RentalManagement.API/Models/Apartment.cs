f60a77b1ddd8ab16f179b716d70e2e18b74ad551
Piotr Kaptur <piotr.kaptur.contact@gmail.com>
2025-06-02 20:07:35 +0200

    Create Apartment model with properties and XML comments, Initial Tenant model with properties

---
+++ RentalManagement.API/Models/Apartment.cs
@@ -0,0 +1,31 @@
+﻿namespace RentalManagement.API.Models
+{
+    /// <summary>
+    /// Reprezentuje mieszkanie dostępne do wynajęcia.
+    /// Zawiera informacje o adresie, piętrze i innych cechach
+    /// </summary>
+    public class Apartment
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Adres mieszkania.
+        /// </summary>
+        public string Address { get; set; } = string.Empty;
+
+        /// <summary>
+        ///  Piętro, na którym znajduje się mieszkanie (0 = parter)
+        /// </summary>
+        public int Floor { get; set; } = 0;
+
+        /// <summary>
+        /// Liczba pokoi w mieszkaniu.
+        /// </summary>
+        public int RoomCount { get; set; }
+
+        /// <summary>
+        /// Cena wynajęcia mieszkania.
+        /// </summary>
+        public decimal RentAmount { get; set; }
+    }
+}
---
+++ RentalManagement.API/Models/Tenant.cs
@@ -0,0 +1,14 @@
+﻿namespace RentalManagement.API.Models
+{
+    /// <summary>
+    /// Reprezentuje lokatora (imie, nazwisko, kontakt itd...)
+    /// </summary>
+    public class Tenant
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
