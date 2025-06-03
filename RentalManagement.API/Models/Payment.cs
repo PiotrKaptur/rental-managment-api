2816146403597cb511edb7d9e2cd074c00c61c27
Piotr Kaptur <piotr.kaptur.contact@gmail.com>
2025-06-03 18:12:28 +0200

    Add Payment model

---
+++ RentalManagement.API/Models/Payment.cs
@@ -0,0 +1,35 @@
+﻿namespace RentalManagement.API.Models
+{
+    /// <summary>
+    /// Reprezentuje pojedyńczą płatność związaną z umową wynajmu.
+    /// </summary>
+    public class Payment
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Data dokonania płatności.
+        /// </summary>
+        public DateTime PaymentDate { get; set; } = DateTime.Today;
+
+        /// <summary>
+        /// Kwota płatności.
+        /// </summary>
+        public decimal Amount { get; set; } = 0.0m;
+
+        /// <summary>
+        /// Komentarz.
+        /// </summary>
+        public string? Comment { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Id umowy wynajmu.
+        /// </summary>
+        public int RentalAgreementId { get; set; }
+
+        /// <summary>
+        /// Płatność przypisana do umowy wynajmu.
+        /// </summary>
+        public RentalAgreement? RentalAgreement { get; set; }
+    }
+}
