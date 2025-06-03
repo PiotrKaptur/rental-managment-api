c02f8fdcf1608067c2ce34e17bdd082532f1b164
Piotr Kaptur <piotr.kaptur.contact@gmail.com>
2025-06-03 17:24:26 +0200

    Add RentalAgreement model and basic properties to Tenant and RentalAgreement

---
+++ RentalManagement.API/Models/RentalAgreement.cs
@@ -0,0 +1,31 @@
+﻿namespace RentalManagement.API.Models
+{
+    /// <summary>
+    /// Reprezentuje umowy wynajęcia
+    /// </summary>
+    public class RentalAgreement
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Data rozpoczęcia umowy (wartość domyślna ustawiona na datę bieżącą)
+        /// </summary>
+        public DateTime StartDate { get; set; } = DateTime.Today;
+
+        /// <summary>
+        /// Data zakonczenia umowy.
+        /// Jeśli null - oznacza umowę bezterminową.
+        /// </summary>
+        public DateTime? EndDate { get; set; }
+
+        /// <summary>
+        /// Miesięczna kwota wynajmu.
+        /// </summary>
+        public decimal MonthlyPayment { get; set; } = 0.0m;
+
+        /// <summary>
+        /// Kod waluty w której naliczana jest opłata. Domyślnie PLN. 
+        /// </summary>
+        public string Currency { get; set; } = "PLN";
+    }
+}
--- RentalManagement.API/Models/Tenant.cs
+++ RentalManagement.API/Models/Tenant.cs
@@ -7,8 +7,24 @@
     {
         public int Id { get; set; }
 
+        /// <summary>
+        /// Imię lokatora.
+        /// </summary>
         public string FirstName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Nazwisko lokatora.
+        /// </summary>
         public string LastName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Numer telefonu lokatora.
+        /// </summary>
         public string PhoneNumber { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Adres e-mail lokatora.
+        /// </summary>
+        public string Email { get; set; } = string.Empty;
     }
 }
