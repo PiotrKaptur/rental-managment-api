fceea8ec84a6d9b23ab2831a05382a86d8a2e3ae
Piotr Kaptur <piotr.kaptur.contact@gmail.com>
2025-06-07 20:00:13 +0200

    Add GET and POST endpoints for Apartment API

---
+++ RentalManagement.API/Controllers/ApartmentController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RentalManagement.API.Data;
+using RentalManagement.API.Models;
+using SQLitePCL;
+
+namespace RentalManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApartmentController : ControllerBase
+    {
+
+        private readonly RentalContext _context;
+
+        public ApartmentController(RentalContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllApartments()
+        {
+            var apartments = _context.Apartments.ToList();
+            return Ok(apartments);
+        }
+
+        [HttpPost]
+        public IActionResult AddApartment([FromBody] Apartment apartment)
+        {
+            _context.Apartments.Add(apartment);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetAllApartments), new { id = apartment.Id }, apartment);
+        }
+    }
+}
