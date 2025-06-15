f067105de1bca830e1756845f7e481894d49ac68
Piotr Kaptur <piotr.kaptur.contact@gmail.com>
2025-06-15 20:33:51 +0200

    status Add Delete in PaymentController

--- RentalManagement.API/Controllers/PaymentController.cs
+++ RentalManagement.API/Controllers/PaymentController.cs
@@ -29,5 +29,11 @@ namespace RentalManagement.API.Controllers
         {
             ;
         }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            ;
+        }
     }
 }
