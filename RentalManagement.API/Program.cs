c5fa594c08d3b9516ccdb3ace0a90e28f01e393e
Piotr Kaptur <piotr.kaptur.contact@gmail.com>
2025-06-21 20:15:15 +0200

    Configure CORS policy to allow any origin, method and header

--- RentalManagement.API/Program.cs
+++ RentalManagement.API/Program.cs
@@ -20,7 +20,23 @@ namespace RentalManagement.API
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy =>
+                {
+                    policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+            });
+
             var app = builder.Build();
+
+
+
+
+
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -36,6 +52,8 @@ namespace RentalManagement.API
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
+
+            app.UseCors("AllowAll");
 
 
             app.MapControllers();
